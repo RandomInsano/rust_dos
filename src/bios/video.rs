9b1cc4b985520277f29bfbe1b7aabd7b456d4e09
Edwin Amsler <EdwinGuy@GMail.com>
2023-02-11 17:25:01 -0600

    Implement VGA DAC setting, various little fixes
    * Renamed set_palette to set_cga_palette
    * Increased width of subroutine names in documentation table
    * Added 320x200 by 256 colour mode (13h)
    * Added the ability to set the RAMDAC palette for VGA graphics mode

--- src/bios/video.rs
+++ src/bios/video.rs
@@ -1,4 +1,4 @@
-//! Video BIOS calls as documented in Phoenix BIOS 4.0 documentation
+//! Video BIOS Interrupt Service Routines
 //! ======================================================================
 //! 
 //! These are the functions for BIOS interrupt 10h functionality (video
@@ -6,25 +6,25 @@
 //! 
 //! Implementation progress:
 //! 
-//! | Op | Function call                                             | Status |
-//! |----|-----------------------------------------------------------|--------|
-//! | 00 | [Set video mode](set_video) (missing VGA modes)           | Partly |
-//! | 01 | [Set cursor size](set_cursor_size)                        |   ✔️    |
-//! | 02 | [Set cursor position](set_cursor_position)                |   ✔️    |
-//! | 03 | Get cursor position                                       |        |
-//! | 04 | Not available                                             |  N/A   |
-//! | 05 | [Change displayed page](set_page)                         |   ✔️    |
-//! | 06 | Scroll active page up                                     |        |
-//! | 07 | Scroll active page down                                   |        |
-//! | 08 | Read character attribute                                  |        |
-//! | 09 | Write character attribute                                 |        |
-//! | 0A | Write character at cursor                                 |        |
-//! | 0B | [Set colour palette](set_palette)                         |   ✔️    |
-//! | 0C | [Write graphics pixel](set_pixel)                         |   ✔️    |
-//! | 0D | [Read graphics pixel](pixel)                              |   ✔️    |
-//! | 0E | Teleytype write character                                 |        |
-//! | 0F | Read current video parameters                             |        |
-//! | 10 | Write string                                              |        |
+//! | Op   | Function call                                           | Status |
+//! |------|---------------------------------------------------------|--------|
+//! | 00   | [Set video mode](set_video) (missing VGA modes)         | Partly |
+//! | 01   | [Set cursor size](set_cursor_size)                      |   ✔️    |
+//! | 02   | [Set cursor position](set_cursor_position)              |   ✔️    |
+//! | 03   | Get cursor position                                     |        |
+//! | 04   | Not available                                           |  N/A   |
+//! | 05   | [Change displayed page](set_page)                       |   ✔️    |
+//! | 06   | Scroll active page up                                   |        |
+//! | 07   | Scroll active page down                                 |        |
+//! | 08   | Read character attribute                                |        |
+//! | 09   | Write character attribute                               |        |
+//! | 0A   | Write character at cursor                               |        |
+//! | 0B   | [Set colour palette](set_cga_palette)                   |   ✔️    |
+//! | 0C   | [Write graphics pixel](set_pixel)                       |   ✔️    |
+//! | 0D   | [Read graphics pixel](pixel)                            |   ✔️    |
+//! | 0E   | Teleytype write character                               |        |
+//! | 0F   | Read current video parameters                           |        |
+//! | 1012 | [Set VGA DAC registers](set_vga_dac)                    |        |
 //! 
 //! ## VESA Extensions
 //! 
@@ -53,6 +53,8 @@
 
 use core::arch::asm;
 
+use crate::dos::misc;
+
 /// Various video modes that can be sent to [set_video]. Check that function
 /// for usage and warnings.
 pub enum VideoMode {
@@ -60,38 +62,52 @@ pub enum VideoMode {
     /// 
     /// Supported adapters: CGA, PCjr, EGA, MCGA, VGA
     Text40_25B = 0x00,
+
     /// Text mode 40x25, 16 colours
     /// 
     /// Supported adapters: CGA, PCjr, EGA, MCGA, VGA
     Text40_25C = 0x01,
+
     /// Text mode 80x25, 16 shades of grey
     /// 
     /// Supported adapters: CGA, PCjr, EGA, MCGA, VGA
     Text80_25B = 0x02,
+
     /// Text mode 80x25, 16 colours
     /// 
     /// Supported adapters: CGA, PCjr, EGA, MCGA, VGA
     Text80_25C = 0x03,
+
     /// Graphics mode 320x200, 4 colours
     /// 
     /// Supported adapters: CGA, PCjr, EGA, MCGA, VGA
     Graphics320_200C2 = 0x04,
+
     /// Graphics mode 320x200, 4 shades of grey
     /// 
     /// Supported adapters: CGA, PCjr, EGA, MCGA, VGA
     Graphics320_200B = 0x05,
+
     /// Graphics mode 640x200, 2 shades of grey
     /// 
     /// Supported adapters: CGA, PCjr, EGA, MCGA, VGA
     Graphics640_200B = 0x06,
+
     /// Text mode 80x25, 2 shades of grey
     /// 
     /// Supported adapters: MDA
     Text20x25B2 = 0x07,
+
     /// Graphics mode 640x400, 16 colours
     ///  
     /// Supported adapters: VGA
     Graphics640x480C4 = 0x12,
+
+    /// Graphics mode 320x200, 16 colours
+    /// 
+    /// Supported adapters: CGA, EGA, VGA
+    Graphics320_200C4 = 0x0d,
+
     /// Graphics mode 320x200, 256 colours
     /// 
     /// Supported adapters: VGA
@@ -164,6 +180,12 @@ impl VesaMode {
 pub enum ColorTarget {
     Background = 0,
     Palette = 1,   
+}
+
+pub struct VgaDacColour {
+    pub red: u8,
+    pub green: u8,
+    pub blue: u8
 }
 
 /// Set the current video mode.
@@ -243,7 +265,7 @@ pub fn set_page(page: u8) {
 }
 
 // Set colour pallet
-pub fn set_palette(target: ColorTarget, color: u8) {
+pub fn set_cga_palette(target: ColorTarget, color: u8) {
     unsafe {
         asm!("mov ah, 0x0b",
             "int 0x10",
@@ -281,4 +303,30 @@ pub fn pixel(page: u8, x: u16, y: u16) -> u8 {
     }
 
     colour
+}
+
+// Set all palette registers
+pub fn set_vga_dac(colours: &[VgaDacColour], start: u32) {
+
+    let (segment, offset) = misc::ptr_to_segments(colours.as_ptr() as u32);
+
+    unsafe {
+        asm!(
+            "mov ax, es",
+            "push ax",          // Preserve data extra segment register
+            "add ax, di",
+            "mov es, ax",       // Offset the segment to where our data is
+
+            "mov ax, 0x1012",
+            "int 0x10",         // Call interrupt
+
+            "pop ax",           // Restore data segment register
+            "mov es, ax",
+
+            in("di") segment,
+            in("dx") offset,
+            in("bx") start,
+            in("cx") colours.len() as u16
+        );
+    }
 }
