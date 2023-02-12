b142cf13b8abef76249b5daccb1bef2ffe042f58
Edwin Amsler <EdwinGuy@GMail.com>
2023-02-11 23:24:06 -0600

    Clean up main function and use new file tests

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ extern crate alloc;
 mod dos_tests;
 
 use rust_dos::*;
-use rust_dos::bios::video::{ColorTarget, VesaMode, VideoMode};
+use rust_dos::bios::video::VesaMode;
 use rust_dos::bios::{
     video,
 };
@@ -17,7 +17,7 @@ use crate::dos_tests::datetime::datetime_test;
 use crate::dos_tests::file::{
     file_attribute_test,
     directory_test,
-    file_read_test,
+    file_read_write_test,
     disk_space_test
 };
 use crate::dos_tests::misc::misc_test;
@@ -39,40 +39,22 @@ const PICTURE_DATA: [[u8; 8]; 8] = [
 
 fn main() {
     // Set resolution to 800x600x8
-    /*
     let mode = VesaMode::new(0x103,
         false,
         true,
         false);
 
     video::set_video_vesa(mode).unwrap();
-    video::set_cursor_size(0x01, 0x0f);
-    video::set_page(1);
-    video::set_palette(ColorTarget::Palette, 5);
-    */
-
-    video::set_video(VideoMode::Graphics320_200C2);
 
     set_verify_writes(true);
 
     let result = Mouse::initialize();
     println!("Mouse mode: {:?}", result);
 
-    Mouse::cursor_show();
-
-    Mouse::set_graphics_cursor(0, 0, &[
-        [0, 0, 0, 0, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff],
-        [0xffff, 0xffff, 0xffff, 0xffff, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    ]);
-
-    Mouse::set_range_vertical(180, 180);
-    Mouse::set_range_horizontal(0, 606);
-
-    /*
     println!("-- Allocator tests");
     allocator_test();
     println!("-- File read tests");
-    file_read_test();
+    file_read_write_test();
     println!("-- File attribute tests");
     file_attribute_test();
     println!("-- Directory tests");
@@ -88,5 +70,4 @@ fn main() {
     print_test();
 
     println!("Write verification status: {}", verify_writes());
-     */
 }
